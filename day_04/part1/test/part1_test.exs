faadffd6393f87659ff6b59155c379ec09a77dff
shenriquesdev <Unisergius@gmail.com>
2023-12-13 08:17:00 +0000

    day 4 part 1 submit

--- day_04/part1/test/part1_test.exs
+++ day_04/part1/test/part1_test.exs
@@ -2,7 +2,15 @@ defmodule Part1Test do
   use ExUnit.Case
   doctest Part1
 
-  test "greets the world" do
-    assert Part1.hello() == :world
+  test "split numbers into integer list" do
+    assert Part1.split_numbers("1 2 3 4") == [1, 2, 3, 4]
+  end
+
+  test "split number into a single integer list" do
+    assert Part1.split_numbers("1") == [1]
+  end
+
+  test "check winning number" do
+    assert Part1.check_winning_number(2, [1, 2, 3]) == 1
   end
 end
